6795931f8dc9cd26acc32f8582ee87b5d89bb79a
Meg Gorregattu <meghanago@gmail.com>
2022-09-20 14:54:10 -0500

    1

---
+++ src/main/java/com/devmountain/skyrimAlchemyApp/SkyrimAlchemyAppApplication.java
@@ -0,0 +1,13 @@
+package com.devmountain.skyrimAlchemyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyrimAlchemyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyrimAlchemyAppApplication.class, args);
+	}
+
+}
